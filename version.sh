2b8cf117d501424075ab67e50170abc27aeca1f2
qxzg <out@qxzg.xyz>
2018-11-29 23:03:33 +0800

    update to frp0.21.0
    clone from https://github.com/clangcn/onekey-install-shell

---
+++ version.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export FRPS_VER=0.20.1
+export FRPS_INIT="https://raw.githubusercontent.com/qxzg/frp-install-shell/master/frps/frps.init"
+export aliyun_download_url="https://code.aliyun.com/clangcn/frp/raw/master"
+export github_download_url="https://github.com/fatedier/frp/releases/download"
