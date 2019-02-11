4c321b80c6ceb6c5e95fe0618db9c8873ed7438e
qxzg <out@qxzg.xyz>
2019-02-11 18:51:54 +0800

    更新至frp 0.24.0

--- version.sh
+++ version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export FRPS_VER=0.23.3
+export FRPS_VER=0.24.0
 export FRPS_INIT="https://raw.githubusercontent.com/qxzg/frp-install-shell/master/frps.init"
 export aliyun_download_url="https://code.aliyun.com/qxzg/frp/raw/master"
 export github_download_url="https://github.com/fatedier/frp/releases/download"
